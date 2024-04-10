e3f4e0d25bb671d229bc728f65e46c038cfb65d6
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-04-10 01:28:19 -0600

    animation: add navigation from top to 0

--- src/gsap.js
+++ src/gsap.js
@@ -1,16 +1,43 @@
-// Preloader Screen 
+// Preloader Screen
 gsap.to(".preloader", {
   x: -5000,
   duration: 2,
-  delay: 5.7
+  delay: 5.7,
 });
 
+// Navigation
+gsap.fromTo(
+  ".navigation",
+  {
+    y: -200,
+  },
+  {
+    y: 0,
+    duration: 1.5,
+    delay: 5.9,
+  }
+);
+
 // Hero Title
-gsap.fromTo(".hero__title__container", { opacity: 0}, { opacity: 1, duration: 1, delay: 6});
+gsap.fromTo(
+  ".hero__title__container",
+  {
+    opacity: 0,
+  },
+  { opacity: 1, duration: 1, delay: 6 }
+);
 
-// Hero Button 
-gsap.fromTo(".hero__button", { opacity: 0}, { opacity: 1, duration: 1, delay: 6.9});
-
-
+// Hero Button
+gsap.fromTo(
+  ".hero__button",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    duration: 1,
+    delay: 6.9,
+  }
+);
 
 
